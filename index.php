bf922d3574a7c2517fc387759d6a5c6b20b5182b
dbrowner1 <111065423+dbrowner1@users.noreply.github.com>
2025-10-21 15:31:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $name = $_SESSION['firstname'] ?? $_SESSION['email'] ?? '';
           <a href="logout.php">Logout</a>
         <?php else: ?>
           <a href="login.php">Login</a>
+          <a href="profile.php">Profile Page</a>
           <a href="register.php">Register</a>
         <?php endif; ?>
       </nav>
